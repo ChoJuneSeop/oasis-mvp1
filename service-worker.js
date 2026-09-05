547f00348f5c094396bc53ad049b19efbb064963
ChoJuneSeop <khai0330@gmail.com>
2026-09-06 07:48:13 +0900

    Cache authoritative OASIS reference core

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE='oasis-integrated-core-v1-final';
-const ASSETS=['./','./index.html','./manifest.json','./icon.svg','./src/oasis-core.mjs','./src/oasis-integrated-core.mjs'];
+const CACHE='oasis-reference-core-v1';
+const ASSETS=['./','./index.html','./manifest.json','./icon.svg','./src/oasis-core.mjs','./src/oasis-integrated-core.mjs','./src/oasis-reference-core.mjs'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',event=>event.respondWith(fetch(event.request,{cache:'no-store'}).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(event.request,copy));return r}).catch(()=>caches.match(event.request))));
