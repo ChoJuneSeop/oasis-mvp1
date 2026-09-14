405f49421691eeaf7b75dda66b66e632e5dd1ff0
ChoJuneSeop <khai0330@gmail.com>
2026-09-14 19:05:32 +0900

    Add v12 operator adapter

--- research/g3_rpfo_v1/entry_v12.py
+++ research/g3_rpfo_v1/entry_v12.py
@@ -2,3 +2,8 @@ from research.g3_rpfo_v1 import rpfo_v12 as base
 
 base.FrontierV12 = base.RelationalFrontierV12
 BaseOperatorV12 = base.RelationalParticipationFoldOperatorV12
+
+
+class OperatorV12(BaseOperatorV12):
+    def __init__(self, resolver):
+        super().__init__(resolver=resolver)
