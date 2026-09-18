f4557a991bcc19b96982357a723eedcd2e4ad10d
ChoJuneSeop <khai0330@gmail.com>
2026-09-19 02:46:11 +0900

    Add Governance OASIS scientific proof harness core

---
+++ research/governance_oasis_scientific_proof_harness_v1/__init__.py
@@ -0,0 +1,29 @@
+"""Scientific proof-design harness for Governance OASIS experiments."""
+
+from .models import (
+    AxisId,
+    CausalContrast,
+    DesignCheck,
+    DesignStatus,
+    ExperimentDesign,
+    EvidenceLevel,
+    EvidenceRecord,
+    PortfolioReport,
+    ProofDesignReport,
+)
+from .design_gate import validate_design
+from .portfolio_gate import audit_portfolio
+
+__all__ = [
+    "AxisId",
+    "CausalContrast",
+    "DesignCheck",
+    "DesignStatus",
+    "ExperimentDesign",
+    "EvidenceLevel",
+    "EvidenceRecord",
+    "PortfolioReport",
+    "ProofDesignReport",
+    "validate_design",
+    "audit_portfolio",
+]
