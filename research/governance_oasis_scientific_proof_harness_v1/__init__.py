ca5e5efda52afc4bae4303878bbd8adfe8edf370
ChoJuneSeop <khai0330@gmail.com>
2026-09-19 03:18:26 +0900

    Expose sequence and three-lens proof gates

--- research/governance_oasis_scientific_proof_harness_v1/__init__.py
+++ research/governance_oasis_scientific_proof_harness_v1/__init__.py
@@ -10,10 +10,14 @@ from .models import (
     EvidenceLevel,
     EvidenceRecord,
     PortfolioReport,
+    ProgramSequenceReport,
     ProofDesignReport,
+    ThreeLensReviewReport,
 )
 from .design_gate import validate_design
 from .portfolio_gate import audit_portfolio
+from .program_sequence import validate_program_sequence
+from .three_lens_gate import three_lens_review
 
 __all__ = [
     "AxisId",
@@ -25,7 +29,11 @@ __all__ = [
     "EvidenceLevel",
     "EvidenceRecord",
     "PortfolioReport",
+    "ProgramSequenceReport",
     "ProofDesignReport",
+    "ThreeLensReviewReport",
     "validate_design",
     "audit_portfolio",
+    "validate_program_sequence",
+    "three_lens_review",
 ]
